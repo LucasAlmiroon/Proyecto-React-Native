c359364e651b0edd891fe50393e94a7fc5676b51
LucasAlmiron <almiron736@gmail.com>
2024-02-13 17:10:23 -0300

    se crea un readme y cerrar sesion

--- screen/Profile.js
+++ screen/Profile.js
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { setToken } from '../features/profile/profileSlice'
 
 const Profile = ({ navigation }) => {
 
     const nombre = useSelector((state) => state.profile.nombre)
     const apellido = useSelector((state) => state.profile.apellido)
     const foto = useSelector((state) => state.profile.foto)
+    const dispatch = useDispatch()
+
+    const cerrarSesion = () => {
+        dispatch(setToken(''))
+    }
+
     return (
         <View style={styles.container}>
 
@@ -28,6 +35,7 @@ const Profile = ({ navigation }) => {
             <Text style={styles.texto}> {nombre} {apellido} </Text>
             <Pressable style={styles.presionable} onPress={() => navigation.navigate('EditProfile')}><Text style={styles.texto}>Editar perfil</Text></Pressable>
             <Pressable style={styles.presionable}><Text style={styles.texto}>Preferencias</Text></Pressable>
+            <Pressable style={styles.presionable} onPress={() => cerrarSesion()}><Text style={styles.texto}>Cerrar Sesion</Text></Pressable>
         </View>
     )
 }
